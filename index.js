f144ff849aacaa0ea78bc4c0e093cb0e1f12657c
Stephanie López <stephanielopez@MacBook-Air-de-Stephanie.local>
2022-06-06 19:59:04 -0500

    prueba inicial

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Header from "./src/React/header/header";
-import Body from "./src/React/body/body";
-import Footer from "./src/React/footer/footer";
+import Prueba from "./src/React/components/prueba/prueba";
 import './src/styles/base.css';
 
 
 export default function App(){
   return(
     <>
-       <Header></Header>
-       <Body></Body>
-       <Footer></Footer>
-    
+      <Prueba/>
     </>
   )
 }
---
+++ src/React/components/prueba/prueba.js
@@ -0,0 +1,11 @@
+
+const Prueba = () =>{
+    return(
+        <>
+            <h2>Prueba</h2>
+        </>
+    )
+
+}
+
+export default Prueba
